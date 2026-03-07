aed2e60c7bf17d55319213b12b0f8a1143e5069a
Wendell Costa <wendell.timoteoc@gmail.com>
2026-03-07 20:25:27 -0300

    feat(ui) - update sections

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,18 @@ tailwind.config = {
         }
     }
 }
+
+$(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            margin: 20,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            responsive:{
+                0:{ items:1 },
+                600:{ items:2 },
+                1000:{ items:4 }
+            }
+        });
+    });
