931ac6b3d374e9459ec6b532e540eda3d899244f
Wendell Costa <wendell.timoteoc@gmail.com>
2026-03-07 19:57:54 -0300

    feat(ui) - create project

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                royal: '#4169E1',
+                darkRoyal: '#1e3a8a',
+            }
+        }
+    }
+}
