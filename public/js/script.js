ed5775b27e736553935caee3d918d147c1c02dff
Wendell Costa <wendell.timoteoc@gmail.com>
2026-03-07 20:31:45 -0300

    feat(ui) - init carousel in sections clients

--- public/js/script.js
+++ public/js/script.js
@@ -10,16 +10,30 @@ tailwind.config = {
 }
 
 $(document).ready(function(){
-        $(".owl-carousel").owlCarousel({
-            loop: true,
-            margin: 20,
-            nav: true,
-            autoplay: true,
-            autoplayTimeout: 3000,
-            responsive:{
-                0:{ items:1 },
-                600:{ items:2 },
-                1000:{ items:4 }
-            }
-        });
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive:{
+            0:{ items:1 },
+            600:{ items:2 },
+            1000:{ items:4 }
+        }
     });
+
+    $("#carousel-clientes-info").owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,  
+        autoplay: true,    
+        autoplayTimeout: 3000,
+        smartSpeed: 800,    
+        responsive:{
+            0:{ items:1 },
+            600:{ items:2 },
+            1000:{ items:4 } 
+        }
+    });
+});
